ed7b5397f94bfd6a4e1fc1205e2e858157b8ff2e
Tana <finaltruth.shintai@gmail.com>
2021-12-11 02:27:59 +0900

    BookDropRateChangerを実装。

---
+++ BookDropRateChanger/Harmony_Patch.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace BookDropRateChanger
+{
+    [Harmony()]
+    public class Harmony_Patch
+    {
+        #region スタートアップ
+
+        /// <summary>
+        /// <see cref="Harmony_Patch"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public Harmony_Patch()
+        {
+            try
+            {
+                var harmony = new Harmony("BookDropRateChanger");
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.ErrorOnExceptionThrown(ex);
+            }
+        }
+
+        #endregion
+
+        #region 本のドロップ数の変更
+
+        /// <summary>設定ファイルのパス</summary>
+        private static readonly string SettingsFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Settings.json");
+
+        /// <summary>変更倍率の最小値</summary>
+        private const float MinRate = 0.0f;
+        /// <summary>変更倍率の最大値</summary>
+        private const float MaxRate = 10.0f;
+        /// <summary>設定ファイルから読み込みできなかった時に使用する規定の変更倍率</summary>
+        private const float DefaultRate = 2.0f;
+        /// <summary>倍率変更後に適応する最低ドロップ数</summary>
+        private const float MinResult = 1.0f;
+
+        /// <summary>
+        /// <see cref="DropTable.CalculateDropProb(float, int)"/> メソッドが呼び出された後に実行されます。
+        /// ドロップする本の数を設定ファイルの変更倍率に従って変更します。
+        /// </summary>
+        /// <param name="__instance">メソッドを呼び出したインスタンス。</param>
+        /// <param name="__result">メソッドの戻り値。ドロップする本の数。</param>
+        /// <param name="originProb"></param>
+        /// <param name="emotionLevel"></param>
+        [HarmonyPatch(typeof(DropTable), "CalculateDropProb")]
+        [HarmonyPostfix]
+        public static void DropTable_CalculateDropProb_Postfix(DropTable __instance, ref float __result, float originProb, int emotionLevel)
+        {
+            try
+            {
+                float rate = LoadDropRate();
+
+                __result *= rate;
+                __result = Mathf.Max(__result, MinResult);
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.ErrorOnExceptionThrown(ex);
+            }
+        }
+
+        /// <summary>
+        /// ドロップ数の変更倍率を設定ファイルから読み込みます。
+        /// </summary>
+        /// <returns></returns>
+        private static float LoadDropRate()
+        {
+            try
+            {
+                string json = File.ReadAllText(SettingsFilePath);
+                var settings = JsonConvert.DeserializeObject<BookDropRateChangerSettings>(json);
+                
+                return Mathf.Clamp(settings.DropRate, MinRate, MaxRate);
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.ErrorOnExceptionThrown(ex);
+                return DefaultRate;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ BookDropRateChanger/Log.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace BookDropRateChanger
+{
+    /// <summary>
+    /// ログ出力機能を提供します。
+    /// </summary>
+    public class Log
+    {
+        #region LogLevel
+
+        /// <summary>
+        /// ログのレベルを表します。
+        /// </summary>
+        private class LogLevel
+        {
+            /// <summary>デバッグ</summary>
+            public static readonly LogLevel Debug = new LogLevel("Debug");
+            /// <summary>情報</summary>
+            public static readonly LogLevel Infomation = new LogLevel("Info");
+            /// <summary>警告</summary>
+            public static readonly LogLevel Warning = new LogLevel("Warn");
+            /// <summary>エラー</summary>
+            public static readonly LogLevel Error = new LogLevel("Error");
+            /// <summary>重大</summary>
+            public static readonly LogLevel Fatal = new LogLevel("Fatal");
+
+            /// <summary>
+            /// ログのレベルを表す文字列を取得します。
+            /// </summary>
+            public string Text { get; }
+
+            /// <summary>
+            /// <see cref="LogLevel"/> の新しいインスタンスを生成します。
+            /// </summary>
+            /// <param name="text">ログのレベルを表す文字列。</param>
+            private LogLevel(string text)
+                => Text = text;
+        }
+
+        #endregion
+
+        #region Singleton 実装
+
+        /// <summary>
+        /// このクラスのインスタンスを取得します。
+        /// 初回呼び出し時にログ ファイルが新規作成されます。既に存在する場合は上書きされます。
+        /// </summary>
+        public static Log Instance { get; } = new Log();
+
+        #endregion
+
+        /// <summary>タイムスタンプの書式指定文字列</summary>
+        private const string TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        /// <summary>ログ ファイルの出力先パス</summary>
+        private readonly string FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "BookDropRateChanger.log");
+
+        /// <summary>
+        /// <see cref="Log"/> の新しいインスタンスを生成します。
+        /// </summary>
+        private Log()
+            => File.WriteAllText(FilePath, "");
+
+        /// <summary>
+        /// 指定したメッセージで情報レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Infomation(string message) 
+            => AddLog(LogLevel.Infomation, message);
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、情報レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void InfomationWithCaller(string message)
+            => Infomation($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        /// <summary>
+        /// 指定したメッセージでエラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Error(string message)
+            => AddLog(LogLevel.Error, message);
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、エラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void ErrorWithCaller(string message)
+            => Error($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        /// <summary>
+        /// 指定した例外の説明メッセージとスタックトレースでエラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力する例外。</param>
+        public void Error(Exception ex)
+            => Error($"{ex.Message}\n{ex.StackTrace}");
+
+        /// <summary>
+        /// 例外がスローされた時の規定のエラー レベルのログを追加します。
+        /// 指定した例外の説明メッセージとスタックトレースがログに追加されます。
+        /// </summary>
+        /// <param name="ex"></param>
+        public void ErrorOnExceptionThrown(Exception ex)
+        {
+            Error($"[{GetCallerInfo(new StackTrace())}] Exception thrown.");
+            Error(ex);
+        }
+
+        /// <summary>
+        /// タイムスタンプと、指定したレベルおよびメッセージをログに追加します。
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="message"></param>
+        private void AddLog(LogLevel level, string message) 
+            => File.AppendAllText(FilePath, $"{DateTime.Now.ToString(TimestampFormat)}|{level.Text,-5}|{message}\n");
+
+        /// <summary>
+        /// 指定したスタック トレースから呼び出し元のクラス名およびメソッド名を返します。
+        /// </summary>
+        /// <param name="trace"></param>
+        /// <returns></returns>
+        private string GetCallerInfo(StackTrace trace)
+        {
+            StackFrame caller = trace.GetFrame(1);
+            if (caller == null) { return "Caller not found"; }
+            return $"{caller.GetMethod().DeclaringType.Name}.{caller.GetMethod().Name}";
+        }
+    }
+}
