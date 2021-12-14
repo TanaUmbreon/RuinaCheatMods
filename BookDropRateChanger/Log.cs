b7f3790f6d1e620b6607ab0402f9b7b02e32bed5
Tana <finaltruth.shintai@gmail.com>
2021-12-15 00:43:50 +0900

    メンバーを追加。リファクタリング。

--- BookDropRateChanger/Log.cs
+++ BookDropRateChanger/Log.cs
@@ -1,49 +1,21 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
-namespace BookDropRateChanger
+namespace TestingAssistGift
 {
     /// <summary>
     /// ログ出力機能を提供します。
     /// </summary>
     public class Log
     {
-        #region LogLevel
-
-        /// <summary>
-        /// ログのレベルを表します。
-        /// </summary>
-        private class LogLevel
-        {
-            /// <summary>デバッグ</summary>
-            public static readonly LogLevel Debug = new LogLevel("Debug");
-            /// <summary>情報</summary>
-            public static readonly LogLevel Infomation = new LogLevel("Info");
-            /// <summary>警告</summary>
-            public static readonly LogLevel Warning = new LogLevel("Warn");
-            /// <summary>エラー</summary>
-            public static readonly LogLevel Error = new LogLevel("Error");
-            /// <summary>重大</summary>
-            public static readonly LogLevel Fatal = new LogLevel("Fatal");
-
-            /// <summary>
-            /// ログのレベルを表す文字列を取得します。
-            /// </summary>
-            public string Text { get; }
-
-            /// <summary>
-            /// <see cref="LogLevel"/> の新しいインスタンスを生成します。
-            /// </summary>
-            /// <param name="text">ログのレベルを表す文字列。</param>
-            private LogLevel(string text)
-                => Text = text;
-        }
-
-        #endregion
-
-        #region Singleton 実装
+        /// <summary>タイムスタンプの書式指定文字列</summary>
+        private const string TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        /// <summary>ログ ファイルの出力先パス</summary>
+        private readonly string FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "BookDropRateChanger.log");
 
         /// <summary>
         /// このクラスのインスタンスを取得します。
@@ -51,32 +23,80 @@ namespace BookDropRateChanger
         /// </summary>
         public static Log Instance { get; } = new Log();
 
-        #endregion
-
-        /// <summary>タイムスタンプの書式指定文字列</summary>
-        private const string TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
-        /// <summary>ログ ファイルの出力先パス</summary>
-        private readonly string FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "BookDropRateChanger.log");
-
         /// <summary>
         /// <see cref="Log"/> の新しいインスタンスを生成します。
         /// </summary>
         private Log()
             => File.WriteAllText(FilePath, "");
 
+        #region Debug
+
+        /// <summary>
+        /// 指定したメッセージでデバッグ レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Debug(string message)
+            => AddLog(LogLevel.Debug, message);
+
+        /// <summary>
+        /// 指定したオブジェクトのフィールドを展開したものをデバッグ レベルのログとして追加します。
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="paramName"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void DebugExpandedFields(object obj, string paramName)
+        {
+            if (paramName == null) { throw new ArgumentNullException(paramName); }
+
+            Info($"{{\"{paramName}\": {Expand(obj)}}}");
+        }
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、デバッグ レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void DebugWithCaller(string message)
+            => Debug($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        #endregion
+
+        #region Info
+
         /// <summary>
         /// 指定したメッセージで情報レベルのログを追加します。
         /// </summary>
         /// <param name="message">出力するメッセージ。</param>
-        public void Infomation(string message) 
-            => AddLog(LogLevel.Infomation, message);
+        public void Info(string message)
+            => AddLog(LogLevel.Info, message);
 
         /// <summary>
         /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、情報レベルのログを追加します。
         /// </summary>
         /// <param name="message">出力するメッセージ。</param>
-        public void InfomationWithCaller(string message)
-            => Infomation($"[{GetCallerInfo(new StackTrace())}] {message}");
+        public void InfoWithCaller(string message)
+            => Info($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        #endregion
+
+        #region Warning
+
+        /// <summary>
+        /// 指定したメッセージで警告レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Warning(string message)
+            => AddLog(LogLevel.Warning, message);
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、警告レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void WarningWithCaller(string message)
+            => Warning($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        #endregion
+
+        #region Error
 
         /// <summary>
         /// 指定したメッセージでエラー レベルのログを追加します。
@@ -110,12 +130,14 @@ namespace BookDropRateChanger
             Error(ex);
         }
 
+        #endregion
+
         /// <summary>
         /// タイムスタンプと、指定したレベルおよびメッセージをログに追加します。
         /// </summary>
         /// <param name="level"></param>
         /// <param name="message"></param>
-        private void AddLog(LogLevel level, string message) 
+        private void AddLog(LogLevel level, string message)
             => File.AppendAllText(FilePath, $"{DateTime.Now.ToString(TimestampFormat)}|{level.Text,-5}|{message}\n");
 
         /// <summary>
@@ -128,6 +150,86 @@ namespace BookDropRateChanger
             StackFrame caller = trace.GetFrame(1);
             if (caller == null) { return "Caller not found"; }
             return $"{caller.GetMethod().DeclaringType.Name}.{caller.GetMethod().Name}";
+        }
+
+        private string Expand(object obj)
+        {
+            switch (obj)
+            {
+                case null:
+                    return "null";
+                case string str:
+                    return $"\"{str.Replace("\"", "\"\"").Replace("\n", "\\n")}\"";
+                case IEnumerable arr:
+                    return $"[{string.Join(", ", ExpandCollection(arr))}]";
+                case int num:
+                    return num.ToString();
+                case float num:
+                    return num.ToString();
+                case bool b:
+                    return b.ToString().ToLower();
+                case Enum e:
+                    return e.ToString();
+                case byte num:
+                    return num.ToString();
+                case short num:
+                    return num.ToString();
+                case long num:
+                    return num.ToString();
+                case double num:
+                    return num.ToString();
+                case DateTime date:
+                    return date.ToString();
+                default:
+                    return $"{{{string.Join(", ", ExpandFields(obj))}}}";
+            }
+        }
+
+        private IEnumerable<string> ExpandFields(object obj)
+        {
+            foreach (FieldInfo member in obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                object memberValue = member.GetValue(obj);
+                if (memberValue == obj) { continue; }
+                yield return $"\"{member.Name}\": {Expand(member.GetValue(obj))}";
+            }
+        }
+
+        private IEnumerable<string> ExpandCollection(IEnumerable arr)
+        {
+            foreach (object obj in arr)
+            {
+                yield return Expand(obj);
+            }
+        }
+
+        /// <summary>
+        /// ログのレベルを表します。
+        /// </summary>
+        private class LogLevel
+        {
+            /// <summary>デバッグ</summary>
+            public static readonly LogLevel Debug = new LogLevel("Debug");
+            /// <summary>情報</summary>
+            public static readonly LogLevel Info = new LogLevel("Info");
+            /// <summary>警告</summary>
+            public static readonly LogLevel Warning = new LogLevel("Warn");
+            /// <summary>エラー</summary>
+            public static readonly LogLevel Error = new LogLevel("Error");
+            /// <summary>重大</summary>
+            public static readonly LogLevel Fatal = new LogLevel("Fatal");
+
+            /// <summary>
+            /// ログのレベルを表す文字列を取得します。
+            /// </summary>
+            public string Text { get; }
+
+            /// <summary>
+            /// <see cref="LogLevel"/> の新しいインスタンスを生成します。
+            /// </summary>
+            /// <param name="text">ログのレベルを表す文字列。</param>
+            private LogLevel(string text)
+                => Text = text;
         }
     }
 }
--- TestingAssistGift/Log.cs
+++ TestingAssistGift/Log.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace TestingAssistGift
@@ -13,40 +12,10 @@ namespace TestingAssistGift
     /// </summary>
     public class Log
     {
-        #region LogLevel
-
-        /// <summary>
-        /// ログのレベルを表します。
-        /// </summary>
-        private class LogLevel
-        {
-            /// <summary>デバッグ</summary>
-            public static readonly LogLevel Debug = new LogLevel("Debug");
-            /// <summary>情報</summary>
-            public static readonly LogLevel Infomation = new LogLevel("Info");
-            /// <summary>警告</summary>
-            public static readonly LogLevel Warning = new LogLevel("Warn");
-            /// <summary>エラー</summary>
-            public static readonly LogLevel Error = new LogLevel("Error");
-            /// <summary>重大</summary>
-            public static readonly LogLevel Fatal = new LogLevel("Fatal");
-
-            /// <summary>
-            /// ログのレベルを表す文字列を取得します。
-            /// </summary>
-            public string Text { get; }
-
-            /// <summary>
-            /// <see cref="LogLevel"/> の新しいインスタンスを生成します。
-            /// </summary>
-            /// <param name="text">ログのレベルを表す文字列。</param>
-            private LogLevel(string text)
-                => Text = text;
-        }
-
-        #endregion
-
-        #region Singleton 実装
+        /// <summary>タイムスタンプの書式指定文字列</summary>
+        private const string TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        /// <summary>ログ ファイルの出力先パス</summary>
+        private readonly string FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestingAssistGift.log");
 
         /// <summary>
         /// このクラスのインスタンスを取得します。
@@ -54,38 +23,133 @@ namespace TestingAssistGift
         /// </summary>
         public static Log Instance { get; } = new Log();
 
-        #endregion
-
-        /// <summary>タイムスタンプの書式指定文字列</summary>
-        private const string TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
-        /// <summary>ログ ファイルの出力先パス</summary>
-        private readonly string FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestingAssistGift.log");
-
         /// <summary>
         /// <see cref="Log"/> の新しいインスタンスを生成します。
         /// </summary>
         private Log()
             => File.WriteAllText(FilePath, "");
 
+        #region Debug
+
+        /// <summary>
+        /// 指定したメッセージでデバッグ レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Debug(string message)
+            => AddLog(LogLevel.Debug, message);
+
+        /// <summary>
+        /// 指定したオブジェクトのフィールドを展開したものをデバッグ レベルのログとして追加します。
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="paramName"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void DebugExpandedFields(object obj, string paramName)
+        {
+            if (paramName == null) { throw new ArgumentNullException(paramName); }
+
+            Info($"{{\"{paramName}\": {Expand(obj)}}}");
+        }
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、デバッグ レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void DebugWithCaller(string message)
+            => Debug($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        #endregion
+
+        #region Info
+
         /// <summary>
         /// 指定したメッセージで情報レベルのログを追加します。
         /// </summary>
         /// <param name="message">出力するメッセージ。</param>
-        public void Infomation(string message)
-            => AddLog(LogLevel.Infomation, message);
+        public void Info(string message)
+            => AddLog(LogLevel.Info, message);
 
         /// <summary>
         /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、情報レベルのログを追加します。
         /// </summary>
         /// <param name="message">出力するメッセージ。</param>
-        public void InfomationWithCaller(string message)
-            => Infomation($"[{GetCallerInfo(new StackTrace())}] {message}");
+        public void InfoWithCaller(string message)
+            => Info($"[{GetCallerInfo(new StackTrace())}] {message}");
 
-        public void Infomation(object obj, string paramName)
+        #endregion
+
+        #region Warning
+
+        /// <summary>
+        /// 指定したメッセージで警告レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Warning(string message)
+            => AddLog(LogLevel.Warning, message);
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、警告レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void WarningWithCaller(string message)
+            => Warning($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        #endregion
+
+        #region Error
+
+        /// <summary>
+        /// 指定したメッセージでエラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void Error(string message)
+            => AddLog(LogLevel.Error, message);
+
+        /// <summary>
+        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、エラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力するメッセージ。</param>
+        public void ErrorWithCaller(string message)
+            => Error($"[{GetCallerInfo(new StackTrace())}] {message}");
+
+        /// <summary>
+        /// 指定した例外の説明メッセージとスタックトレースでエラー レベルのログを追加します。
+        /// </summary>
+        /// <param name="message">出力する例外。</param>
+        public void Error(Exception ex)
+            => Error($"{ex.Message}\n{ex.StackTrace}");
+
+        /// <summary>
+        /// 例外がスローされた時の規定のエラー レベルのログを追加します。
+        /// 指定した例外の説明メッセージとスタックトレースがログに追加されます。
+        /// </summary>
+        /// <param name="ex"></param>
+        public void ErrorOnExceptionThrown(Exception ex)
         {
-            if (paramName == null) { throw new ArgumentNullException(paramName); }
+            Error($"[{GetCallerInfo(new StackTrace())}] Exception thrown.");
+            Error(ex);
+        }
 
-            Infomation($"{{\"{paramName}\": {Expand(obj)}}}");
+        #endregion
+
+        /// <summary>
+        /// タイムスタンプと、指定したレベルおよびメッセージをログに追加します。
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="message"></param>
+        private void AddLog(LogLevel level, string message)
+            => File.AppendAllText(FilePath, $"{DateTime.Now.ToString(TimestampFormat)}|{level.Text,-5}|{message}\n");
+
+        /// <summary>
+        /// 指定したスタック トレースから呼び出し元のクラス名およびメソッド名を返します。
+        /// </summary>
+        /// <param name="trace"></param>
+        /// <returns></returns>
+        private string GetCallerInfo(StackTrace trace)
+        {
+            StackFrame caller = trace.GetFrame(1);
+            if (caller == null) { return "Caller not found"; }
+            return $"{caller.GetMethod().DeclaringType.Name}.{caller.GetMethod().Name}";
         }
 
         private string Expand(object obj)
@@ -140,69 +204,32 @@ namespace TestingAssistGift
         }
 
         /// <summary>
-        /// 指定したメッセージで警告レベルのログを追加します。
+        /// ログのレベルを表します。
         /// </summary>
-        /// <param name="message">出力するメッセージ。</param>
-        public void Warning(string message)
-            => AddLog(LogLevel.Warning, message);
-
-        /// <summary>
-        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、警告レベルのログを追加します。
-        /// </summary>
-        /// <param name="message">出力するメッセージ。</param>
-        public void WarningWithCaller(string message)
-            => Warning($"[{GetCallerInfo(new StackTrace())}] {message}");
-
-        /// <summary>
-        /// 指定したメッセージでエラー レベルのログを追加します。
-        /// </summary>
-        /// <param name="message">出力するメッセージ。</param>
-        public void Error(string message)
-            => AddLog(LogLevel.Error, message);
-
-        /// <summary>
-        /// 指定したメッセージにこのメソッドの呼び出し元情報を追加して、エラー レベルのログを追加します。
-        /// </summary>
-        /// <param name="message">出力するメッセージ。</param>
-        public void ErrorWithCaller(string message)
-            => Error($"[{GetCallerInfo(new StackTrace())}] {message}");
-
-        /// <summary>
-        /// 指定した例外の説明メッセージとスタックトレースでエラー レベルのログを追加します。
-        /// </summary>
-        /// <param name="message">出力する例外。</param>
-        public void Error(Exception ex)
-            => Error($"{ex.Message}\n{ex.StackTrace}");
-
-        /// <summary>
-        /// 例外がスローされた時の規定のエラー レベルのログを追加します。
-        /// 指定した例外の説明メッセージとスタックトレースがログに追加されます。
-        /// </summary>
-        /// <param name="ex"></param>
-        public void ErrorOnExceptionThrown(Exception ex)
+        private class LogLevel
         {
-            Error($"[{GetCallerInfo(new StackTrace())}] Exception thrown.");
-            Error(ex);
-        }
+            /// <summary>デバッグ</summary>
+            public static readonly LogLevel Debug = new LogLevel("Debug");
+            /// <summary>情報</summary>
+            public static readonly LogLevel Info = new LogLevel("Info");
+            /// <summary>警告</summary>
+            public static readonly LogLevel Warning = new LogLevel("Warn");
+            /// <summary>エラー</summary>
+            public static readonly LogLevel Error = new LogLevel("Error");
+            /// <summary>重大</summary>
+            public static readonly LogLevel Fatal = new LogLevel("Fatal");
 
-        /// <summary>
-        /// タイムスタンプと、指定したレベルおよびメッセージをログに追加します。
-        /// </summary>
-        /// <param name="level"></param>
-        /// <param name="message"></param>
-        private void AddLog(LogLevel level, string message) 
-            => File.AppendAllText(FilePath, $"{DateTime.Now.ToString(TimestampFormat)}|{level.Text,-5}|{message}\n");
+            /// <summary>
+            /// ログのレベルを表す文字列を取得します。
+            /// </summary>
+            public string Text { get; }
 
-        /// <summary>
-        /// 指定したスタック トレースから呼び出し元のクラス名およびメソッド名を返します。
-        /// </summary>
-        /// <param name="trace"></param>
-        /// <returns></returns>
-        private string GetCallerInfo(StackTrace trace)
-        {
-            StackFrame caller = trace.GetFrame(1);
-            if (caller == null) { return "Caller not found"; }
-            return $"{caller.GetMethod().DeclaringType.Name}.{caller.GetMethod().Name}";
+            /// <summary>
+            /// <see cref="LogLevel"/> の新しいインスタンスを生成します。
+            /// </summary>
+            /// <param name="text">ログのレベルを表す文字列。</param>
+            private LogLevel(string text)
+                => Text = text;
         }
     }
 }
